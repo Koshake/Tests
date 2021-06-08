856700749fc7b2db30a6363bf8e3e57d90295643
Koshake <koshake1@mail.com>
2021-06-08 09:16:02 +0300

    initial

---
+++ app/src/main/java/com/geekbrains/tests/presenter/details/PresenterDetailsContract.kt
@@ -0,0 +1,11 @@
+package com.geekbrains.tests.presenter.details
+
+import com.geekbrains.tests.presenter.PresenterContract
+
+internal interface PresenterDetailsContract : PresenterContract {
+    fun setCounter(count: Int)
+    fun onIncrement()
+    fun onDecrement()
+    fun onAttach()
+    fun onDetach()
+}
