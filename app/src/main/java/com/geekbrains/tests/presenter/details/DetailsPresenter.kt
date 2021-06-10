187596ef2b49d2d040389610454f3634b9437c47
Koshake <koshake1@mail.com>
2021-06-10 08:53:26 +0300

    presenter onAttach/onDetach presenter tests

--- app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
@@ -3,9 +3,10 @@ package com.geekbrains.tests.presenter.details
 import com.geekbrains.tests.view.details.ViewDetailsContract
 
 internal class DetailsPresenter internal constructor(
-    private val viewContract: ViewDetailsContract,
     private var count: Int = 0
 ) : PresenterDetailsContract {
+
+    var viewContract: ViewDetailsContract? = null
 
     override fun setCounter(count: Int) {
         this.count = count
@@ -13,19 +14,23 @@ internal class DetailsPresenter internal constructor(
 
     override fun onIncrement() {
         count++
-        viewContract.setCount(count)
+        viewContract?.setCount(count)
     }
 
     override fun onDecrement() {
         count--
-        viewContract.setCount(count)
+        viewContract?.setCount(count)
     }
 
-    override fun onAttach() {
-        TODO("Not yet implemented")
+    override fun onAttach(view: ViewDetailsContract) {
+        if (view != viewContract) {
+            viewContract = view
+        }
     }
 
-    override fun onDetach() {
-        TODO("Not yet implemented")
+    override fun onDetach(view: ViewDetailsContract) {
+        if (viewContract == view) {
+            viewContract = null
+        }
     }
 }
--- app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
+++ app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
@@ -5,6 +5,7 @@ import com.geekbrains.tests.model.SearchResult
 import com.geekbrains.tests.presenter.search.SearchPresenter
 import com.geekbrains.tests.repository.GitHubRepository
 import com.geekbrains.tests.view.search.ViewSearchContract
+import org.junit.Assert
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
@@ -30,7 +31,8 @@ class SearchPresenterTest {
         //Раньше было @RunWith(MockitoJUnitRunner.class) в аннотации к самому классу (SearchPresenterTest)
         MockitoAnnotations.initMocks(this)
         //Создаем Презентер, используя моки Репозитория и Вью, проинициализированные строкой выше
-        presenter = SearchPresenter(viewContract, repository)
+        presenter = SearchPresenter(repository)
+        presenter.onAttach(viewContract)
     }
 
     @Test //Проверим вызов метода searchGitHub() у нашего Репозитория
@@ -148,5 +150,16 @@ class SearchPresenterTest {
 
         //Убеждаемся, что ответ от сервера обрабатывается корректно
         verify(viewContract, times(1)).displaySearchResults(searchResults, 101)
+    }
+
+    @Test
+    fun presenter_onDetach_Test() {
+        presenter.onDetach(viewContract)
+        Assert.assertNull(presenter.viewContract)
+    }
+
+    @Test
+    fun presenter_OnAttach_Test() {
+        Assert.assertNotNull(presenter.viewContract)
     }
 }
